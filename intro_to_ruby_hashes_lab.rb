5faa2f3e95cd1ae40d4e9787616805867dfbfb84
Amy Nguyen <amy.nguyen730@gmail.com>
2019-09-16 21:31:56 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,9 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	new_hash = {}
+	monopoly = {
+	railroads: {}
+}
 end
 
 def monopoly_with_second_tier
@@ -10,8 +12,12 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	monopoly = {
-		:railroad => {}
-	}
+    railroads: {
+      pieces: 4,
+      names: {},
+      rent_in_dollars: {}
+    }
+  }
 end
 
 def monopoly_with_third_tier
@@ -20,15 +26,15 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	monopoly = {
-		:railroad => {
+		railroads: {
 			pieces: 4,
-			name: {
-				reading_railroad:{},
-				pennsylvania: {},
-				b_and_o_railroad: {},
-				shortline: {}
-			}
-			rent_in_dollars: {one_piece_owned: 25, two_piece_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
+			names: {
+				:reading_railroad => {},
+				:pennsylvania_railroad => {},
+				:b_and_o_railroad => {},
+				:shortline_railroad => {}
+			},
+			rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
 		}
 	}
 end
@@ -39,15 +45,15 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	monopoly = {
-		:railroad => {
+		railroads: {
 			pieces: 4,
-			name: {
-				reading_railroad:{"mortage value" => "$100"},
-				pennsylvania: {"mortage value" => "$200"},
-				b_and_o_railroad: {"mortage value" => "$400"},
-				shortline: {"mortage value" => "$800"}
+			names: {
+				:reading_railroad => {"mortgage_value" => "$100"},
+				:pennsylvania_railroad => {"mortgage_value" => "$200"},
+				:b_and_o_railroad => {"mortgage_value" => "$400"},
+				:shortline_railroad => {"mortgage_value" => "$800"}
 			},
-			rent_in_dollars: {one_piece_owned: 25, two_piece_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
+			rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
 		}
 	}
 end
